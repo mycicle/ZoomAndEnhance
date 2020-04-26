bf2262a31044a26011e929f0d65a8f22c25acea5
danielcooke <46968051+danielcooke@users.noreply.github.com>
2020-04-26 13:50:49 -0400

    yeet

--- matlab_photo_mosaic/photomosaic_bw.m
+++ matlab_photo_mosaic/photomosaic_bw.m
@@ -22,8 +22,9 @@ for i = 1:Size_Index(2)
     for j = 1:Size_Index(1)
         n = 0;
         while Photo_Index(j, i) == 0
-            Random = randi([1 int16(Num_Files)]);
-            for k = Random:Num_Files
+            Random_1 = randi([1, int16(Num_Files)]);
+            Random_2 = randi([1, int16(Num_Files)]);
+            for k = min(Random_1, Random_2):max(Random_1, Random_2)
                 Difference = (abs(int32(Frame_Index(1,k)) - int32(Color_Index(j, i))));
                 if (Difference <= n)
                     Photo_Index(j, i) = k;
@@ -43,18 +44,18 @@ for i = 1:Size_Index(2)
         Min_Size = min(Size_Add_IMG(1),Size_Add_IMG(2));
         Square_IMG = zeros(Min_Size,Min_Size, 'uint8');
         for k = 1:Min_Size
-            for l = 1:Min_Size
+            for r = 1:Min_Size
                 Vertical_Correction = ((Size_Add_IMG(1)-Min_Size)/2);
                 Horizontal_Correction = ((Size_Add_IMG(2)-Min_Size)/2);
-                Square_IMG(k,l) = Add_IMG((k + Vertical_Correction), (l + Horizontal_Correction));
+                Square_IMG(k,r) = Add_IMG((k + Vertical_Correction), (r + Horizontal_Correction));
             end
         end
         Small_x = (Size_IMG(2)/num_photos_x);
         Small_y = (Size_IMG(1)/num_photos_y);
         Small_IMG = imresize(Square_IMG, [ Small_y Small_x]);
         for k = 1:Small_y
-            for l = 1:Small_x
-                Output((l+((j-1)*Small_x)), (k+((i-1)*Small_y))) = Small_IMG(k,l);
+            for r = 1:Small_x
+                Output((r+((j-1)*Small_x)), (k+((i-1)*Small_y))) = Small_IMG(r,k);
             end
         end
     end
--- matlab_photo_mosaic/photomosaic_color.m
+++ matlab_photo_mosaic/photomosaic_color.m
@@ -24,16 +24,17 @@ for i = 1:Size_Index(2)
     for j = 1:Size_Index(1)
             n = 0;
             while Photo_Index(j, i) == 0
-                Random = randi([1, int16(Num_Files)]);
-                for l = Random:int16(Num_Files)
-                    Difference_R =(abs(int32(Frame_Index(1, l, 1)) - int32(Color_Index(j, i, 1))));
-                    Difference_G =(abs(int32(Frame_Index(1, l, 2)) - int32(Color_Index(j, i, 2))));
-                    Difference_B =(abs(int32(Frame_Index(1, l, 3)) - int32(Color_Index(j, i, 3))));
+                Random_1 = randi([1, int16(Num_Files)]);
+                Random_2 = randi([1, int16(Num_Files)]);
+                for r = min(Random_1, Random_2):max(Random_1, Random_2)
+                    Difference_R =(abs(int32(Frame_Index(1, r, 1)) - int32(Color_Index(j, i, 1))));
+                    Difference_G =(abs(int32(Frame_Index(1, r, 2)) - int32(Color_Index(j, i, 2))));
+                    Difference_B =(abs(int32(Frame_Index(1, r, 3)) - int32(Color_Index(j, i, 3))));
                     if (Difference_R <= n && Difference_G <= n && Difference_B <= n)
-                        Photo_Index(j, i) = l;
+                        Photo_Index(j, i) = r;
                         break
                     else
-                        n = n + 3;
+                        n = n + 1;
                     end
                 end
             end
@@ -47,11 +48,11 @@ for i = 1:Size_Index(2)
         Min_Size = min(Size_Add_IMG(1),Size_Add_IMG(2));
         Square_IMG = zeros(Min_Size, Min_Size, 3, 'uint8');
         for k = 1:Min_Size
-            for l = 1:Min_Size
+            for r = 1:Min_Size
                 Vertical_Correction = ((Size_Add_IMG(1)-Min_Size)/2);
                 Horizontal_Correction = ((Size_Add_IMG(2)-Min_Size)/2);
                 for m = 1:3
-                    Square_IMG(k, l, m) = Add_IMG((k + Vertical_Correction), (l + Horizontal_Correction), m);
+                    Square_IMG(k, r, m) = Add_IMG((k + Vertical_Correction), (r + Horizontal_Correction), m);
                 end
             end
         end
@@ -59,9 +60,9 @@ for i = 1:Size_Index(2)
         Small_y = (Size_IMG(1)/num_photos_y);
         Small_IMG = imresize(Square_IMG, [ Small_y Small_x]);
         for k = 1:Small_y
-            for l = 1:Small_x
+            for r = 1:Small_x
                 for m = 1:3
-                    Output((l+((j-1)*Small_x)), (k+((i-1)*Small_y)), m) = Small_IMG(k, l, m);
+                    Output((r+((j-1)*Small_x)), (k+((i-1)*Small_y)), m) = Small_IMG(r, k, m);
                 end
             end
         end
